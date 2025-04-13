99621ffdebf6e4c1f3504abb8d0e614610c927b0
tandaddos <tandaddos@gmail.com>
2025-04-12 19:31:21 -0700

    add to options.lua

--- nvim/.config/nvim/lua/config/core/options.lua
+++ nvim/.config/nvim/lua/config/core/options.lua
@@ -5,6 +5,7 @@ vim.g.maplocalleader = " "
 vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt
+
 -- tabs/indents
 opt.shiftwidth = 4
 opt.tabstop = 4
@@ -26,12 +27,20 @@ opt.relativenumber = true
 -- background/foreground color per colorsheme
 opt.termguicolors = true
 opt.background = "dark" -- prefer dark
-opt.signcolumn = "yes"  -- turn on sign column text doesn't shift
+opt.signcolumn = "yes"  -- turn on sign column so text doesn't shift
 
 -- sessions
 opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+opt.undofile = true -- store undos between sessions
 
--- misc
+-- window splits
+opt.splitright = true
+opt.splitbelow = true
+
+opt.cursorline = true   -- show line under cursor``
+opt.cursorcolumn = true -- show column
+opt.breakindent = true
+
 opt.backspace = "indent,eol,start"  -- allow backspace in these cases
 opt.clipboard:append("unnamedplus") -- add system clipboard as default
 opt.wrap = false
@@ -40,10 +49,21 @@ opt.hidden = false         -- Required to keep multiple buffers open multiple bu
 opt.encoding = "utf-8"     -- The encoding displayed
 opt.fileencoding = "utf-8" -- The encoding written to file
 opt.ruler = true           -- Show the cursor position all the time
-opt.iskeyword:append("-")  -- treat dash separated words as a word text object--
-opt.mouse = "nr"           -- Enable your mouse
-opt.updatetime = 300       -- Faster completion
-opt.timeoutlen = 500       -- By default timeoutlen is 1000 ms
+
+-- how whitespaces are displayed
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+opt.inccommand = "split" -- preview substitutions live, as you type
+opt.cmdheight = 0        -- disable commandline till needed
+
+-- misc
+opt.iskeyword:append("-") -- treat dash separated words as a word text object--
+opt.mouse = "nr"          -- Enable your mouse
+opt.updatetime = 300      -- Faster completion
+opt.timeoutlen = 500      -- By default timeoutlen is 1000 ms
+
 -- opt.formatoptions:append("-cro") -- Stop newline continuation of comments
-opt.autochdir = false      -- Your working directory will always be the same as your working directory
-opt.title = true           -- title automaticallyopt.to filename--
+
+opt.autochdir = false -- Your working directory will always be the same as your working directory
+opt.title = true      -- title automatically
